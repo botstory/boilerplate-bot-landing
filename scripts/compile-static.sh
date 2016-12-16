bf269a5c7b8655491431a8c0e62162d97bc5d000
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2016-12-16 09:05:03 +0100

    fix url to assets

--- scripts/compile-static.sh
+++ scripts/compile-static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "[ ] Compiling Static ..."
 
-PUBLIC_PATH="${PROJECT_NAME}" npm run build:prod
+PUBLIC_PATH="/${PROJECT_NAME}" npm run build:prod
 
 echo "[ ] Static is compiled ..."
